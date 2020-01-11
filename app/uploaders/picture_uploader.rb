4b086cb67b0d7c65325892d075704d26f2aa3fa9
EC2 Default User <ec2-user@ip-172-31-39-63.ap-northeast-1.compute.internal>
2020-01-11 12:51:27 +0000

    暫定レイアウト

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -37,7 +37,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-    %w(jpeg png)
+    %w(jpeg png jpg)
   end
 
   # Override the filename of the uploaded files:
