418807592e1925b8ac208889afda95288df40c08
EC2 Default User <ec2-user@ip-172-31-39-63.ap-northeast-1.compute.internal>
2020-01-17 06:53:31 +0000

    テストの変数名を変更

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -3,11 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  let(:tester_for_like) { FactoryBot.create(:user) }
-  let(:post_for_like) { FactoryBot.create(:post, user: tester_for_like) }
+  let(:user) { FactoryBot.create(:user) }
+  let(:post) { FactoryBot.create(:post, user: user) }
 
   it 'いいね,いいね取り消しができること' do
-    expect { tester_for_like.good(post_for_like) }.to change { tester_for_like.liked_posts.count }.by(1)
-    expect { tester_for_like.not_good(post_for_like) }.to change { tester_for_like.liked_posts.count }.by(-1)
+    expect { user.good(post) }.to change { user.liked_posts.count }.by(1)
+    expect { user.not_good(post) }.to change { user.liked_posts.count }.by(-1)
   end
 end
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -3,15 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  let(:tester_for_follow) { FactoryBot.create(:user) }
-  let(:other_tester_for_follow) { FactoryBot.create(:user) }
+  let(:tester) { FactoryBot.create(:user) }
+  let(:other_tester) { FactoryBot.create(:user) }
+  
   it 'フォローするとfollowingが増えること、その取り消しができること' do
-    expect { tester_for_follow.follow(other_tester_for_follow) }.to change { tester_for_follow.following.count }.by(1)
-    expect { tester_for_follow.unfollow(other_tester_for_follow) }.to change { tester_for_follow.following.count }.by(-1)
+    expect { tester.follow(other_tester) }.to change { tester.following.count }.by(1)
+    expect { tester.unfollow(other_tester) }.to change { tester.following.count }.by(-1)
   end
   
   it 'フォローされるとfollowersが増えること、その取り消しができること' do
-    expect { other_tester_for_follow.follow(tester_for_follow) }.to change { tester_for_follow.followers.count }.by(1)
-    expect { other_tester_for_follow.unfollow(tester_for_follow) }.to change { tester_for_follow.followers.count }.by(-1)
+    expect { other_tester.follow(tester) }.to change { tester.followers.count }.by(1)
+    expect { other_tester.unfollow(tester) }.to change { tester.followers.count }.by(-1)
   end
 end
