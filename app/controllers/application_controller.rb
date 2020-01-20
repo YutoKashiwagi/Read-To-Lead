b32611be904505c8fc7bf33bedf50ee20e3dd538
EC2 Default User <ec2-user@ip-172-31-39-63.ap-northeast-1.compute.internal>
2020-01-20 09:23:10 +0000

    検索機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :set_search
+  
   def configure_permitted_parameters
     # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
@@ -12,5 +13,10 @@ class ApplicationController < ActionController::Base
     #編集時
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:profile])
+  end
+  
+  def set_search
+    @search = Post.ransack(params[:q])
+    @search_posts = @search.result
   end
 end
